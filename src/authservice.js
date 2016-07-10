c134e0028f74dd7ff171d2a7192fb05c61da7853
waynepennington <wayne.pennington@gmail.com>
2016-07-10 11:54:33 -0400

    shorten

--- src/authservice.js
+++ src/authservice.js
@@ -1,19 +1,18 @@
 import {keycloak} from './keycloak';
-import {noView} from 'aurelia-framework';
 
 export class AuthService {
 
     configure(config){
         var installURL;
-        if ( typeof config.install == 'undefined'){
-            installURL = 'keycloak.json';
-        }else{
-            installURL = config.install;           
-        }
-        let keycloak = new Keycloak(installURL);
-        
+        // if ( typeof config.install == 'undefined'){
+        //     installURL = 'keycloak.json';
+        // }else{
+        //     installURL = config.install;           
+        // }
+        // let keycloak = new Keycloak(installURL);
+        let auth = new Keycloak(config.install);
         if ( typeof config.initOption !== 'undefined'){
-            this.keycloak.init(config.initOptions);
+            this.auth.init(config.initOptions);
         }
     }
 }
