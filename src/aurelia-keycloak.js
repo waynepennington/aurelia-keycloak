031bff42f0d93d671e9a262564fbc66ab40928ec
waynepennington <wayne.pennington@gmail.com>
2016-07-10 10:09:49 -0400

    clean house 1

--- src/aurelia-keycloak.js
+++ src/aurelia-keycloak.js
@@ -4,20 +4,5 @@ import {AuthService} from './authservice';
 export function configure(aurelia, config) {
     let instance = aurelia.container.get(AuthService);
     instance.configure(config);
-    
-//authservice = new AuthService(config);
-//aurelia.globalName('./aurelia-keycloak/aurelia-keycloak','aurelia-keycloak');
-//aurelia.container.registerSingleton('AuthService', authservice);
-}
 
 export {AuthService}
-        
-// aurelia.container.registerSingleton('keycloak', Keycloak);
-//keycloak(config.install);
-//let keycloak = aurelia.container.get(Keycloak);
-//export function configure(aurelia, configCallback) {
-//            let keycloak = new Keycloak();
-//            aurelia.container.registerSingleton('keycloak',keycloak);
-//                configCallback(keycloak);
-//        }
-
--- src/authservice.js
+++ src/authservice.js
@@ -2,38 +2,19 @@ import {keycloak} from './keycloak';
 import {noView} from 'aurelia-framework';
 
 export class AuthService {
-    constructor(config){
-        this.keycloak = null;
-      }
-      configure(config){
+    keycloak = null;
+
+    configure(config){
         var installURL;
         if ( typeof config.install == 'undefined'){
             installURL = 'keycloak.json';
         }else{
             installURL = config.install;           
         }
-        this.keycloak = Keycloak(installURL);
+        this.keycloak = new Keycloak(installURL);
         
         if ( typeof config.initOption !== 'undefined'){
             this.keycloak.init(config.initOptions);
         }
-      }
+    }
 }
-
-
-//import {Keycloak} from './keycloak';
-//export class Config {
-//    constructor(){
-//        this.keycloak = null;
-//    }
-//    newKeycloak(config){
-//        this.keycloak = new Keycloak();
-//    }
-//    init(initOptions){
-//        this.keycloak.init(initOptions);
-//        return this;
-//    }
-//    get(){
-//        return this.keycloak;
-//    }
-//    }
