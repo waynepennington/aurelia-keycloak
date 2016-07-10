02cfe0a53ea30f1909cba14db4a91e28288efa5f
waynepennington <wayne.pennington@gmail.com>
2016-07-09 23:41:24 -0400

    experiment number 2

---
+++ src/authservice.js
@@ -0,0 +1,40 @@
+import {keycloak} from './keycloak';
+import {noView} from 'aurelia-framework';
+
+@noView
+export class AuthService {
+    constructor(config){
+        this.keycloak = null;
+      }
+      configure(config){
+        var installURL;
+        if ( typeof config.install == 'undefined'){
+            installURL = 'keycloak.json';
+        }else{
+            installURL = config.install;           
+        }
+        this.keycloak = Keycloak(installURL);
+        
+        if ( typeof config.initOption !== 'undefined'){
+            this.keycloak.init(config.initOptions);
+        }
+      }
+}
+
+
+//import {Keycloak} from './keycloak';
+//export class Config {
+//    constructor(){
+//        this.keycloak = null;
+//    }
+//    newKeycloak(config){
+//        this.keycloak = new Keycloak();
+//    }
+//    init(initOptions){
+//        this.keycloak.init(initOptions);
+//        return this;
+//    }
+//    get(){
+//        return this.keycloak;
+//    }
+//    }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import {Keycloak} from './keycloak';
+import {AuthService} from './authservice';
 
-export function configure(aurelia, configCallback) {
-  let keycloak = aurelia.container.get(Keycloak);
-
-  if (configCallback !== undefined && typeof(configCallback) === 'function') {
-      configCallback(keycloak);
-  }
-  aurelia.globalResources('./aurelia-keycloak');
+//var authservice;
+export function configure(aurelia, config) {
+    let instance = aurelia.container.get(AuthService);
+    instance.configure(config);
+    
+//authservice = new AuthService(config);
+//aurelia.globalName('./aurelia-keycloak/aurelia-keycloak','aurelia-keycloak');
+//aurelia.container.registerSingleton('AuthService', authservice);
 }
 
-export {Keycloak}
+export {AuthService}
+        
+// aurelia.container.registerSingleton('keycloak', Keycloak);
+//keycloak(config.install);
+//let keycloak = aurelia.container.get(Keycloak);
+//export function configure(aurelia, configCallback) {
+//            let keycloak = new Keycloak();
+//            aurelia.container.registerSingleton('keycloak',keycloak);
+//                configCallback(keycloak);
+//        }
+
