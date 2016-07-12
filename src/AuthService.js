6614a325cab75f6dc52eabb10745e214fd91cf3b
waynepennington <wayne.pennington@gmail.com>
2016-07-11 20:26:48 -0400

    new day

--- src/AuthService.js
+++ src/AuthService.js
@@ -1,13 +1,32 @@
 import {keycloak} from './keycloak';
 
 export class AuthService { 
-    constructor(keycloak){
-        let keycloak = keycloak;
-    }        
+    static init(){
+        this.Keycloak = null;
+    }
+    constructor(){
+        // this.Keycloak = null;
+        this.loadKeycloakScript();
+        console.log('INFO keycloak.js loaded');
+    }
     configure(config){
-        keycloak(config.install);
-        if (typeof config.initOption !== 'undefined') {
-            this.keycloak.init(config.initOptions);
+        this.Keycloak = new Keycloak(config.install);
+        console.log('INFO Keycloak authentication client installation configuration loaded');
+        if (typeof config.initOptions !== 'undefined') {
+            this.Keycloak.init(config.initOptions);
+            console.log('INFO Keycloak initialization options loaded');
+            console.log('INFO ' + config.initOptions);                                     
         }
+    }
+    loadKeycloakScript() {
+        if (window.Keycloak === undefined) {
+            let script = document.createElement('script');
+
+            script.type = 'text/javascript';
+            script.async = false;
+            script.defer = false;
+            script.src = `./dist/aurelia-keycloak/keycloak.js`;
+            document.body.appendChild(script);
+        }    
     }
 }
--- src/aurelia-keycloak.js
+++ src/aurelia-keycloak.js
@@ -1,8 +1,10 @@
-import {AuthService} from './AuthService';
+import {AuthService} from './aurelia-keycloak';
 
 export function configure(aurelia, config) {
     let instance = aurelia.container.get(AuthService);
     instance.configure(config);
+    console.log('INFO AuthService configured'); 
+    aurelia.globalResources('./aurelia-keycloak');
 }
 
 export {AuthService}
